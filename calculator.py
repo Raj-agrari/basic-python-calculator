52df0600dc22912a823c1fa569245fc5ae39a5f9
Raj-agrari <159036998+Raj-agrari@users.noreply.github.com>
2024-02-23 01:19:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+
+
+print("select operation")
+print("1 addition")
+print("2 subtraction")
+print("3 multiplication")
+print("4 divide")
+
+choice = input("enter your choice (1,2,3,4):  \n")
+
+num1 = float(input("enter your number: "))
+num2 = float(input("enter your number: "))
+
+if choice == '1':
+    print(num1+num2)
+
+elif choice == '2':
+    print(num1-num2)
+
+elif choice == '3':
+    print(num1*num2)
+
+elif choice == '4':
+    print(num1/num2)
+
+else:
+    print("invalid output")
+
+
